debdadb3881439695aaa4b39a27fcbb1f0a2e370
hakazescarlet <frost.scarlet.163@gmail.com>
2023-04-10 19:10:18 +0300

    added toString method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         CsvParser parser = new CsvParser();
-        parser.parse();
+        List<User> users = parser.parse();
+        for (User user : users) {
+//            System.out.println(user.getName());
+//            System.out.println(user.getSurname());
+//            System.out.println(user.getEmail());
+            System.out.println(user);
+        }
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -27,4 +27,13 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
