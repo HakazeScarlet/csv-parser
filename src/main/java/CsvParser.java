06e68b1a159acaaef44b7ba3b68df2d220df58dd
hakazescarlet <frost.scarlet.163@gmail.com>
2023-04-10 19:19:46 +0300

    added option to skip the first line in csvReader

--- src/main/java/CsvParser.java
+++ src/main/java/CsvParser.java
@@ -8,12 +8,16 @@ import java.util.List;
 
 public class CsvParser {
 
+    private static final int NUMBER_OF_LINES_TO_SKIP = 1;
+
     public List<User> parse() {
         List<User> users = new ArrayList<>();
         try (
                 Reader reader = Files.newBufferedReader(Path.of(CsvParser.class.getResource("emails.csv").toURI()));
                 CSVReader csvReader = new CSVReader(reader)
         ) {
+            csvReader.skip(NUMBER_OF_LINES_TO_SKIP);
+
             String[] line;
             while ((line = csvReader.readNext()) != null) {
                 User user = new User();
